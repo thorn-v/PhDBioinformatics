03ca967714d6dab56f46392737eb794c2b3a7d48
thorn-v <vthorn@uoguelph.ca>
2023-01-05 16:01:13 -0500

    Added functions to main script and added SNV call

--- snpCallingPipline.sh
+++ snpCallingPipline.sh
@@ -3,7 +3,6 @@
 
 script_name=$0
 script_full_path=$(dirname $0)
-source ${script_full_path}/helperFunctions.sh
 
 # These are the files and variables that will be needed
 usage() { printf 'Varient Calling Pipleine V1
@@ -18,8 +17,8 @@ usage() { printf 'Varient Calling Pipleine V1
         Assumes you have fastp, BWA mem, sra-toolbox,    
 
         -i\tThe SRA accesson (folder that contains the Raw sequencing files) [REQUIRED]
-        -r\tSequence Reference library Folder [REQUIRED]
-        -o\tThe output prefix (Default: inputfoldername_processed)
+        -r\tSequence Reference library Folder path [REQUIRED]
+        -o\tThe output prefix (Default: inputfoldername)
 	-n\tNumber of CPU Threads to be used (Default: 8)
 	-l\tLog File Name (Default: $date)
         -q\tMinimum Mapping Quality (Default: 30)
@@ -27,13 +26,15 @@ usage() { printf 'Varient Calling Pipleine V1
         -h\tShow this help message and exit\n' 1>&2; exit 1; }
 
 # Creating a simple command to save the settings used.
-log() {	printf "Mapping settings for $(date):
-	Log File:\t${log}
-	Input folder:\t${folder}
-	Output folder:\t${out}
-	CPU Threads:\t${ncores}
-	-------------------------------------\n"; exit 0;
-}
+#log() {	printf "Mapping settings for $(date):
+#	Log File:\t${log}
+#	Input folder:\t${folder}
+#	Output folder:\t${out}
+#	CPU Threads:\t${ncores}
+#	-------------------------------------\n"; exit 0;
+#}
+
+## add logging feature later
 
 #export -f Trimming
 #export -f FastpWrapper
@@ -51,10 +52,10 @@ while getopts "i:r:k:n:o:l:h" arg; do
         case $arg in
                 i)
                         declare -r sample=${OPTARG}
-                        out= printf "${OPTARG}_processed"
+                        out= printf "${OPTARG}"
                         ;;
                 r)
-                        declare -r reference=${OPTARG}
+                        declare -r ref=${OPTARG}
                         ;;
                 o)
                         out=${OPTARG}
@@ -90,22 +91,18 @@ if [[ ! -d "${sample}" ]]; then #checks that the sample is a folder and not the 
         exit 1;
 fi
 
-if [[ -z "${reference}" ]]; then
+if [[ -z "${ref}" ]]; then
         printf '\nMissing required input - Please provide Reference Genome Library\n\nUse -h for usage help\n'
         exit 1;
 fi
 
 
 
-echo $sample
-echo $reference   
-#echo $ncores
-#echo $len
-#echo $log
-# Writing the Log File
-#log | tee $log
 
 #### Extract the fastq(s) ####
+module load StdEnv/2020
+module load gcc/9.3.0
+module load sra-toolkit
 
 fasterq-dump $sample -O "./${out}" &
 
@@ -118,43 +115,126 @@ cd "${out}" #move into the newley created directory with the fastq file(s)
 # find the newly created files since they could have different ways of denoting r1/r2
 
 findExtension ${sample}
-# now we should have r1 and r2 (could be NA)
+# now we should have r1 and r2 (could be NA) as global variables
 
 # call fastp wrapper
 
-Trimming "${r1}" "${r2}" "${sample}" "${out}"
+fileArray=($sample*)
+    # echo "${fileArray[@]}"
+    # Identifying the files since they can have different endings when dumped
+if  printf '%s\n' "${fileArray[@]}" | grep -E -i -q  "r1\.f*|_1\.f*|_r1_0.*|_1"; then
+        r1=$(printf '%s\n' "${fileArray[@]}" | grep -E -i 'r1\.f*|_1\.f*|_r1_0.*|_1')
+else 
+        r1="NA"
+fi
+
+# for r2
+if printf '%s\n' "${fileArray[@]}" | grep -E -i -q "r2\.f*|_2\.f*|_r2_0.*|2$"; then
+        r2=$(printf '%s\n' "${fileArray[@]}" | grep -E -i 'r2\.f*|_2\.f*|_r2_0.*|2$')
+else
+        r2="NA"     
+fi
+#echo $r2
+unset fileArray
+
+
+## QC and Trims the fastq file(s) based on Sam Long's parameters 
+
+if [ "$r2" == "NA" ]; then # If not a paired sample...
+
+        fastp -i $r1 \
+                --out1 ${out}_r1_trimmed.fastq \
+                --low_complexity_filter --correction \
+                --cut_right --cut_right_window_size 4 --cut_right_mean_quality 15\
+                --cut_front --cut_front_window_size 1 --cut_front_mean_quality 3\
+                --cut_tail --cut_tail_window_size 1 --cut_tail_mean_quality 3\
+                --length_required $len\
+                --html ${out}FastpLogs/${sample}.html -R $sample\
+                --json ${out}FastpLogs/${sample}.json -R $sample \
+                --failed_out ${out}FailedQC/${sample}_failed.fastq.gz;
+
+else  # is a paired sample
+		
+        fastp -i $r1 -I $r2 \
+		--out1 ${out}Trimmed/${sample}_r1_trimmed.fastq \
+		--out2 ${out}Trimmed/${sample}_r2_trimmed.fastq \
+                --detect_adapter_for_pe --correction --low_complexity_filter \
+                --cut_right --cut_right_window_size 4 --cut_right_mean_quality 15\
+                --cut_front --cut_front_window_size 1 --cut_front_mean_quality 3\
+                --cut_tail --cut_tail_window_size 1 --cut_tail_mean_quality 3\
+                --overlap_len_require 15 --length_required $len\
+                --html ${out}FastpLogs/${sample}.html -R $sample \
+                --json ${out}FastpLogs/${sample}.json -R $sample \
+                --unpaired1 ${out}Trimmed/${sample}_u1.fastq.gz \
+                --unpaired2 ${out}Trimmed/${sample}_u2.fastq.gz\
+                --failed_out ${out}FailedQC/${sample}_failed.fastq.gz;
+fi
+
 
 
 ###### Mapping ######
 
 if [[ "${r1}" == "NA" && "${r2}" == "NA" ]]; then  
-        printf "${1}\tno r files found - possibly merged?" | tee -a missingFiles.txt
+        printf "${sample}\tno reads files found - possibly merged?" | tee -a missingFiles.txt
         exit 1;
 fi
 
 
 if [[ "$r1" != "NA" && "$r2" == "NA" ]]; then # If it is not paired reads
-        bwa mem $ref $r1 -t $ncores |\
-                samtools view -b -h -F 4 -m $len -q $qual -U tmp.bam |\ #bam, w/ headers, exclude unmapped, include only greater len than $len (30 default), Skip alignments with MAPQ smaller than $qual (default 30), add unincluded to diff file 
-                samtools sort -o tmpSingle.bam 
+        bwa mem ${ref} \
+                ${out}Trimmed/${sample}_r1_trimmed.fastq \ 
+                -t $ncores | samtools view -b -h -F 4 -m ${len} -q ${qual} -U tmp.bam |\ #bam, w/ headers, exclude unmapped, include only greater len than $len (30 default), Skip alignments with MAPQ smaller than $qual (default 30), add unincluded to diff file 
+                samtools sort - > ${out}MappedReads/${sample}_mapped.bam 
 
         samtools fastq tmp.bam | gzip > ${out}UnmappedReads/${sample}_Single.fastq.gz
 fi
 
-if [[ "$r1" != "NA" && "$r2" != "NA" ]]; then # If I found a merged file
+if [[ "$r1" != "NA" && "$r2" != "NA" ]]; then # if paird
 
-        bwa mem $ref $r1 $r2 -t $ncores |\
-                samtools view -b -h -F 4 -m $len -q $qual -U tmp.bam |\ 
-                samtools sort -o tmpP.bam
+        bwa mem ${ref} ${out}Trimmed/${sample}_r1_trimmed.fastq \
+                ${out}Trimmed/${sample}_r2_trimmed.fastq -t ${ncores} |\
+                samtools view -b -h -F 4 -m ${len} -q ${qual} -U tmp.bam |\ 
+                samtools sort - > ${out}MappedReads/${sample}_mapped.bam
 
-        samtools fastq -c 6 tmp.bam -1 ${out}UnmappedReads/${sample}_r1.fastq.gz -2 ${out}UnmappedReads/${sample}_r2.fastq.gz -s /dev/null
+        samtools fastq -c 6 tmp.bam -1 ${out}UnmappedReads/${sample}_r1.fastq.gz -2 ${out}UnmappedReads/${sample}_r2.fastq.gz -s /dev/null # deletes singleton readings
 
 fi
 
+##### Processing ######
+
+cd ${out}MappedReads
+module load picard
+module load samtools
+
+picard SortSam \
+      I=${sample}_mapped.bam \
+      O=${sample}_sorted.bam \
+      SORT_ORDER=coordinate
+
+picard MarkDuplicates \
+      I=${sample}_sorted.bam \
+      O=${sample}_sorted-md.bam \
+      M=${sample}-md_metrics.txt
+
+samtools index ${sample}_sorted-md.bam
+
+cd ..
+
+###### Varient Calling ########
+
+freebayes-parallel \
+   <(fasta_generate_regions.py ${ref}.fai 100000) ${ncores} \
+   -f ${ref} -p 1 ${out}MappedReads/${sample}_sorted-md.bam > ${sample}.vcf
 
 
+####### Filter VCF ##########
 
+mkdir FilteredVCF
 
+module load vcftools
 
-
+# separate indels
+vcftools --vcf ${sample}.vcf --keep-only-indels --recode --recode-INFO-all --out FilteredVCF/${sample}_indels-only.vcf
+# separate SNPs
+vcftools --vcf ${sample}.vcf --remove-indels --recode --recode-INFO-all --out FilteredVCF/${sample}_snps-only.vcf
 
