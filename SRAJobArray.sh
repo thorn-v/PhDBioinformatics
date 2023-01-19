b754e9fd9c22257b0b4ca0be922db90f61162e8c
thorn-v <vthorn@uoguelph.ca>
2023-01-19 13:08:34 -0500

    tried to fix run error

--- SRAJobArray.sh
+++ SRAJobArray.sh
@@ -18,5 +18,5 @@ sample=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $c
 # Print to a file a message that includes the current $SLURM_ARRAY_TASK_ID, the same name, and the sex of the sample
 # echo "This is array task ${SLURM_ARRAY_TASK_ID} and the sample name is ${sample}." >> arrayOutput.txt
 
-srun snpCallingPipeline.sh -i ~/scratch/Afumigatus_WGSA_SRAs/${sample} -r ~/scratch/Afumigatus_Reference/A_fumigatus_Af293/GCA_000002655.1/GCA_000002655.1_ASM265v1_genomic.fna; rm -r ~/scratch/Temp_WD/"${sample}"*
+sh ~/scratch/snpCallingPipeline.sh -i ~/scratch/Afumigatus_WGSA_SRAs/${sample} -r ~/scratch/Afumigatus_Reference/A_fumigatus_Af293/GCA_000002655.1/GCA_000002655.1_ASM265v1_genomic.fna; rm -r ~/scratch/Temp_WD/"${sample}"*
 
