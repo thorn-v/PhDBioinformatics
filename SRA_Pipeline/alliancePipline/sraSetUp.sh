a458d79e730b3a20ee0c080c388edd0af8a57404
thorn-v <thornv2@mcmaster.ca>
2025-03-20 08:18:21 -0400

    tuned to work better on cedar

--- SRA_Pipeline/alliancePipline/sraSetUp.sh
+++ SRA_Pipeline/alliancePipline/sraSetUp.sh
@@ -2,21 +2,25 @@
 
 #SBATCH --job-name=SRAsetup
 #SBATCH -A def-jpxu
-#SBATCH --cpus-per-task=32  #can change
-#SBATCH --time=03:00:00     #can change
-#SBATCH --mem-per-cpu=2G    #can change
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=48  #can change
+#SBATCH --time=12:00:00     #can change
+#SBATCH --mem-per-cpu=4000    #can change
+## ^ currently takes advantage of a whole node on cedar, may not need as much with lower amount of samples
 
-module load sra-toolkit
+
+#using the old version as the newer one kept throwing errors
+module load StdEnv/2020  gcc/9.3.0
+module load sra-toolkit/2.10.8
 
 downloadSRA() {
     # Download Script
     prefetch $1 -O SRAs/$1
-    fasterq-dump SRA/$1 -O Raw_Fastqs                   
-    gzip Raw_Fastqs/${1}.*        #gzip extracted fastq(s)
+    fasterq-dump ${1} -O Raw_Fastqs/${1}                   
+    gzip Raw_Fastqs/${1}/*        #gzip extracted fastq(s)
     echo "${1} complete"
 }
 export -f downloadSRA
 
+# if your job runs out of time before finishing, add --resume before --joblog and re-run 
 parallel -j $SLURM_CPUS_PER_TASK --joblog sras.log downloadSRA {} :::: ./accs.txt
-
-
