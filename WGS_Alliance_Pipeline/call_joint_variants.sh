0eabe17b6a49d04a3b806fb34a22897904cc13f1
thorn-v <thornv2@mcmaster.ca>
2025-05-07 11:59:30 -0400

    new pipeline for more general WGS

---
+++ WGS_Alliance_Pipeline/call_joint_variants.sh
@@ -0,0 +1,54 @@
+#! /usr/bin/env bash
+#SBATCH --job-name=combined_calling
+#SBATCH -A def-jpxu
+#SBATCH --time=01:00:00   #can change
+#SBATCH --mem=6G          #can change
+## can add  #SBATCH --mail-type=ALL  if you want it to email you updates
+
+## Default Values
+ploidy=1
+chromList=chroms.list
+MAP=samples.map
+databaseName=wgs_database  #change if you want; must not already exist
+OUT=combined_raw_dataset   #can change if want
+
+if [[ ! -e "${chromList}" ]]; then #if file does not exist
+        echo "Provided interval file: ${chromList} cannot be found\nPlease provide path to file."
+        exit 1;
+fi
+
+if [[ ! -e "${MAP}" ]]; then
+        echo "Reference file: ${MAP} cannot be found\nPlease provide path to sample map."
+        exit 1;
+fi
+
+if [[ -e "${databaseName}" ]]; then
+        echo "Database already exists, GATK will not work\nPlease delete it or provide new name."
+        exit 1;
+fi
+
+
+module load gatk
+javamem=$((SLURM_MEM_PER_NODE-1024)) #need to make sure there will be enough memory in mb
+export JAVA_TOOL_OPTIONS="-Xmx${javamem}m"
+
+#------------------------------------------------
+#                Import Database
+#------------------------------------------------
+gatk --java-options "-Xmx${javamem}m" GenomicsDBImport \
+       --genomicsdb-workspace-path ${databaseName} \
+       -L ${chromList} \
+       --sample-name-map ${MAP}
+
+#------------------------------------------------
+#                Call Combined VCF
+#------------------------------------------------
+gatk --java-options "-Xmx${javamem}m" GenotypeGVCFs \
+       -R ${REF} \
+       -V gendb://${databaseName} \
+       -O ${OUT}.vcf.gz \
+       -L $chromList \
+       -ploidy $ploidy \
+       -stand-call-conf 20
+
+
---
+++ WGS_Alliance_Pipeline/housekeeping.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+#SBATCH --job-name=housekeeping
+#SBATCH -A def-jpxu
+#SBATCH --time=00:30:00     #can change
+#SBATCH --mem=1G #can change
+
+REF=PATH/TO/FASTA/REF.fasta
+# Download the reference from NCBI. For example for A. fumigatus Af290:
+# rsync -rv rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/655/GCF_000002655.1_ASM265v1/ .
+
+module load bwa
+module load samtools
+module load picard
+
+bwa index ${REF}
+samtools faidx ${REF}
+java -jar $EBROOTPICARD/picard.jar CreateSequenceDictionary \
+      R=${REF} \
+      O=${REF}.dict
+
+# Create Chromosome List
+grep "^>" ${REF} | cut -d " " -f 1 | sed -e 's/>//g' > chroms.list
+
