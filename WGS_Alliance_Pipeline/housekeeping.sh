0e3008de1afc871dddc61b506c2f6a3f34d95667
thorn-v <thornv2@mcmaster.ca>
2025-06-11 11:04:13 -0400

    fixed dict file output

--- WGS_Alliance_Pipeline/housekeeping.sh
+++ WGS_Alliance_Pipeline/housekeeping.sh
@@ -17,7 +17,7 @@ bwa index ${REF}
 samtools faidx ${REF}
 java -jar $EBROOTPICARD/picard.jar CreateSequenceDictionary \
       R=${REF} \
-      O=${REF}.dict
+      O=${REF%.f*}.dict
 
 # Create Chromosome List
 grep "^>" ${REF} | cut -d " " -f 1 | sed -e 's/>//g' > chroms.list
