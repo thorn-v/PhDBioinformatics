1cbd84b65b1d06e67c936eb064e56fbe3657ffbd
thorn-v <thornv2@mcmaster.ca>
2025-10-01 13:12:52 -0400

    removed hardcoded inputs to SortVCF

--- WGS_Alliance_Pipeline/gvcf_parallel.sh
+++ WGS_Alliance_Pipeline/gvcf_parallel.sh
@@ -1,59 +1,93 @@
 #! /usr/bin/env bash
 
 #SBATCH --job-name=parallel_gvcf
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task 4 #needs to be the same amonut of chromosomes
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task 14 #needs to be the same amonut of chromosomes / should match the number of chromosomes in the chroms.list
 #SBATCH --mem-per-cpu=8G #Needs to be 4G abosulte min, realistically should be around 8G+ depending on depths.
 #SBATCH --time=300
-## can add  #SBATCH --mail-type=ALL  if you want it to email you updates
+#SBATCH --array=1-??%100 # !! replace the ?? with the total number of samples you have (number of lines in your configfile). if you have less than 100 samples, remove the "%100"
+
+#config file for accessions
+#should be a list of unique parts of file names for each sample - one per line
+config=~/scratch/PATH/TO/configfile.txt
+# if your file is names something other than configfile.txt, then use your filename.
+
+# get the accession from the list
+ACC=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $config)
+#if testing on one accession before an array, comment the above and uncomment the below, putting the accession # directly
+#ACC="SRR12345"
+
+
+#ref should be bwa index, samtools index, and gatk dict (have run housekeeping.sh)
+#!! needs to have gatk dict run on it vv
+REF=~/PATH/TO/REF #### <<<---- put path to ref here
 
 ## Default Values
 ploidy=1
 chromList=chroms.list
+#if your chromosome list is named something else or in a different directory, fix it above
 
-while getopts "a:r:h" arg; do
-        case $arg in
-                a)
-                        ACC=${OPTARG}
-                        ;;
-                r)
-                        REF=${OPTARG}
-                        ;;
-                h | *)
-                        echo "Make sure -r and -a are specified"
-                        exit 0
-                        ;;
-        esac
-done
-
-
-javamem=$((SLURM_MEM_PER_NODE-2048)) 
+javamem=$((SLURM_MEM_PER_NODE-2000)) 
 #need to make sure there will be enough memory -
 #SLURM_MEM_PER_NODE reports in Mb so equivelent to total memory allocated less 1Gb to make sure enough extra space for Java. 
 
 ###### Varient Calling ########
 mkdir -p GVCFs
 
+module load gatk
+module load picard
+export JAVA_TOOL_OPTIONS="-Xmx${javamem}m"
+
 #TODO change this to scatter/gather option with SortVCF
 
 scatter(){
 # assumes gatk is in bin, if its not, manually set path here. !!!
-gatk --java-options "-Xmx${javamem}g" HaplotypeCaller \
+gatk --java-options "-Xmx${javamem}m" HaplotypeCaller \
         -ERC GVCF \
         -L ${1} \
         -ploidy ${ploidy} \
         -R ${3} \
-        -I MappedReads/${2}/${2}_sorted-md.bam \
-        -O GVCFs/${2}/${2}_${1}.g.vcf.gz 
+        -I MappedReads/${2}_sorted-md.bam \
+        -O GVCFs/temp/${2}_${1}.g.vcf.gz 
 }
 export -f scatter
 
-parallel -j $SLURM_CPUS_PER_TASK --joblog comnine_${ACC}.log scatter {} ${ACC} ${REF} :::: ./chromList
+parallel -j $SLURM_CPUS_PER_TASK --joblog combnine_${ACC}.log scatter {} ${ACC} ${REF} :::: $chromList
 
-
+wait
 #===========================
-#     Gather
+#     Gatherx
 #===========================
+readarray -t chroms < $chromList
 
+# Join with spaces
+IFS=' ' commandGuts="I=GVCFs/temp/${ACC}_${chroms[*]}.g.vcf.gz"
 
+        # I=GVCFs/temp/${ACC}_${chroms[0]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[1]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[2]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[3]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[4]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[5]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[6]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[7]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[8]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[9]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[10]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[11]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[12]}.g.vcf.gz \
+        # I=GVCFs/temp/${ACC}_${chroms[13]}.g.vcf.gz \
+
+#currently hardcoded for 14 chromosomes (C neoformans)
+java -jar $EBROOTPICARD/picard.jar SortVCF \
+        ${commandGuts} \
+        O="GVCFs/${ACC}.g.vcf.gz"
+
+if [[ -e GVCFs/${ACC}.g.vcf.gz ]]; then
+        printf "${ACC}\tGVCFs/${ACC}.g.vcf.gz\n" | tee -a samples.map
+        rm GVCFs/temp/${ACC}.*
+else
+        echo "Something went wrong while making the GVCF - Check it out!"
+        exit 1
+fi
 
